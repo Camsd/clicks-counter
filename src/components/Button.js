595087ab8d317c44b68717bed523a891009f0529
Eduard Camilo Ortega Sanchez <codecos.info@gmail.com>
2023-04-24 08:52:17 -0500

    Button component

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Button(props){
+    return(
+        <button className={ props.isClickButton ? "click-button" : "restart-button" }
+        onClick={props.clickEvent}>
+            {props.text}
+        </button>
+    )
+}
+
+export default Button;
